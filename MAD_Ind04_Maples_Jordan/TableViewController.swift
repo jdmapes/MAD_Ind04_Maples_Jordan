e70f1b766d1f7925962f7bf546196fac85467110
Jordan Maples <jordan.d.maples@gmail.com>
2022-04-05 20:00:28 -0500

    Finalized comments and Made sure all works in TVC

--- MAD_Ind04_Maples_Jordan/TableViewController.swift
+++ MAD_Ind04_Maples_Jordan/TableViewController.swift
@@ -16,12 +16,6 @@ class TableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
         
         self.stateView.fetchgetStateinfo(completion: self.tableViewController.reloadData)
     }
@@ -40,7 +34,6 @@ class TableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "My Table Cell", for: indexPath)
-//        cell.backgroundColor = UIColor(red: 1.0, green: 0.5, blue: 0, alpha: 1.0)
 
         // Grab data for the cell from the tuple
         let cellData = stateView.cellForRowAt(indexPath: indexPath)
@@ -48,57 +41,10 @@ class TableViewController: UITableViewController {
         // Configure the cell...
         cell.textLabel?.text = cellData.state
         cell.detailTextLabel?.text = cellData.nickname
-        
-//        cell.textLabel?.text = StatesResult.init(states: [String])
-//        cell.detailTextLabel?.text = States.nickname
 
         return cell
     }
  
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 }
 // Assigning the values from the JSON
 struct StatesResult: Decodable {
@@ -177,6 +123,7 @@ class StateViewModel {
     private var apiService = ApiService()
     private var statesListInfo = [States]()
     
+    // Returning a list of states from the parse if successful
     func fetchgetStateinfo(completion: @escaping () -> ()) {
         apiService.getStateInfo { (result) in
             switch result {
@@ -189,6 +136,7 @@ class StateViewModel {
         }
     }
     
+    // Setting Num of rows based on states list return count
     func numberOfRowsInSection(section: Int) -> Int {
         if statesListInfo.count != 0 {
             return statesListInfo.count
